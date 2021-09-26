44ecdf5890e2e007621426af3a7eccf4e25ba2bf
aggarwalsneha <88716378+aggarwalsneha@users.noreply.github.com>
2021-09-26 14:33:25 +0530

    Add files via upload

---
+++ Music Player/data.js
@@ -0,0 +1,92 @@
+let songs = [
+    {
+        name: 'Baatein Kuch Ankahee Si',
+        path: 'Songs\Baatein Kuch Ankahee Si.mp3',
+        artist: 'artist 1',
+        cover: 'Images\Cover1.png'
+    },
+    {
+        name: 'Badan Pe Sitare Lapete Huye',
+        path: 'Songs\Badan Pe Sitare Lapete Huye.mp3',
+        artist: 'artist 2',
+        cover: 'Images\Cover2.png'
+    },
+    {
+        name: 'Song',
+        path: 'Songs\Song.mp3',
+        artist: 'artist 3',
+        cover: 'Images\Cover3.png'
+    },
+    {
+        name: 'Slow down',
+        path: 'Songs\Slow down.mp3',
+        artist: 'artist 4',
+        cover: 'Images\Cover4.png'
+    },
+    {
+        name: 'Bg Tune',
+        path: 'Songs\Bg Tune.mp3',
+        artist: 'artist 5',
+        cover: 'Images\Cover5.png'
+    },
+    {
+        name: 'Carton-On and On',
+        path: 'Songs\Carton-On and On.mp3',
+        artist: 'artist 6',
+        cover: 'Images\Cover6.png'
+    },
+    {
+        name: 'Shape of You',
+        path: 'Songs\Shape of You.mp3',
+        artist: 'artist 7',
+        cover: 'Images\Cover7.png'
+    },
+    {
+        name: 'Makhna',
+        path: 'Songs\Makhna.mp3',
+        artist: 'artist 8',
+        cover: 'Images\Cover8.png'
+    },
+    {
+        name: 'Closer - The Chainsmokers',
+        path: 'Songs\Closer - The Chainsmokers.mp3',
+        artist: 'artist 9',
+        cover:'Images\Cover9.jpg'
+    },
+    {
+        name: 'Happy',
+        path: 'Songs\Happy.mp3',
+        artist: 'artist 10',
+        cover: 'Images\Cover10.jpg'
+    },
+    {
+        name: 'Jab Koi Baat',
+        path: 'Songs\Jab Koi Baat.mp3',
+        artist: 'artist 11',
+        cover: 'Images\Cover11.jpg'
+    },
+    {
+        name: 'Lag Ja Gale',
+        path: 'Songs\Lag Ja Gale.mp3',
+        artist: 'artist 12',
+        cover: 'Images\Cover12.jpg'
+    },
+    {
+        name: 'Let Me Love You',
+        path: 'Songs\Let Me Love You.mp3',
+        artist: 'artist 13',
+        cover: 'Images\Cover13.png'
+    },
+    {
+        name: 'Love Me Like You Do',
+        path: 'Songs\Love Me Like You Do.mp3',
+        artist: 'artist 14',
+        cover: 'Images\Cover14.jpg'
+    },
+    {
+        name: 'Magical',
+        path: 'Songs\Magical.mp3',
+        artist: 'artist 15',
+        cover: 'Images\Cover15.jpg'
+    },
+]
